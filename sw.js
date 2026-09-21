6629e29411f9dd25f3a3b4c9a988ff00ed7386e6
Manuella Sampaio <manuella.sampaio@estudante.ifms.edu.br>
2026-09-21 10:56:33 -0400

    feat: add sw installation, activation and fetch events

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+    console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service Worker ativado");
+});
+
+self.addEventListener("fetch", event => {
+    console.log("URL:");
+    console.log(event.request.url);
+
+    console. log("Método:");
+    console.log(event.request.method) ;
+
+    if (event.request.url.endsWith("/teste-sw")) {
+        event. respondWith(new Response("Resposta criada pelo Service Worker!"));
+        return;
+    }
+});
